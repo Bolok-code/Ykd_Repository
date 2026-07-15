6cbac6e060641a97acc503996c74305ad550b559
ddds <2727486033@qq.com>
2026-07-15 21:14:33 +0800

    试试

--- src/main/java/com/youkeda/exercise/shared/diyike123/Diyike123Application.java
+++ src/main/java/com/youkeda/exercise/shared/diyike123/Diyike123Application.java
@@ -2,7 +2,7 @@ package com.youkeda.exercise.shared.diyike123;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//yuanshenniubi
 @SpringBootApplication
 public class Diyike123Application {
 
