95a1a3ef346eeb514a745d700780b3840b521729
bolok <2650602812@qq.com>
2026-07-15 14:29:06 +0800

    测试

---
+++ ykd/src/main/java/ykd/ykd/YkdApplication.java
@@ -0,0 +1,13 @@
+package ykd.ykd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YkdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YkdApplication.class, args);
+    }
+
+}
