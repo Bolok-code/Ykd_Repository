a52e0efa5c60e9ae885b0e1b720b1df3445c6fc0
L <1483842068@qq.com>
2026-07-15 17:28:16 +0800

    helloworld后面的数字改为666

--- ykd/src/main/java/ykd/ykd/Test.java
+++ ykd/src/main/java/ykd/ykd/Test.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Test {
 @GetMapping("/Hello")
     public String   Hello(){
-    return "HelloWorld666";
+    return "HelloWorld777";
 }
 }
 
