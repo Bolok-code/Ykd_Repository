207dca1a645cc311183aaec5955b5ac645f984f6
dingdingdashi <2727486033@qq.com>
2026-07-15 11:52:49 +0800

    Initial commit

---
+++ src/main/java/com/youkeda/exercise/shared/diyike123/Diyike123Application.java
@@ -0,0 +1,13 @@
+package com.youkeda.exercise.shared.diyike123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Diyike123Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Diyike123Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/youkeda/exercise/shared/diyike123/Diyike123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youkeda.exercise.shared.diyike123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Diyike123ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
